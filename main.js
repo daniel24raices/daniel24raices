3ef2517518756d94f7cc5631f831aece047982e7
Daniel Raices <daniel24raices@gmail.com>
2024-03-14 10:54:43 +0100

    js desplazamiento y links

--- main.js
+++ main.js
@@ -17,3 +17,39 @@
         var imageText = this.nextElementSibling;
         imageText.classList.toggle("hidden");
     });
+
+    function smoothScroll(targetId) {
+        const target = document.getElementById(targetId);
+        const offsetTop = target.offsetTop;
+        const scrollDuration = 1000; // Duración del desplazamiento en milisegundos
+        let startTime = null;
+        const startPosition = window.pageYOffset;
+    
+        function animation(currentTime) {
+            if (startTime === null) {
+                startTime = currentTime;
+            }
+            const timeElapsed = currentTime - startTime;
+            const scrollProgress = Math.min(timeElapsed / scrollDuration, 1); // Garantiza que el progreso no exceda 1
+            const easeInOutQuad = t => t<.5 ? 2*t*t : -1+(4-2*t)*t; // Función de interpolación easing
+    
+            window.scrollTo(0, startPosition + (offsetTop * easeInOutQuad(scrollProgress)));
+    
+            if (timeElapsed < scrollDuration) {
+                requestAnimationFrame(animation);
+            } else {
+                window.location.hash = targetId; // Restablecer la ubicación hash para permitir el scroll hacia arriba
+            }
+        }
+    
+        requestAnimationFrame(animation);
+    }
+    
+    // Detectar todos los enlaces <a> y agregar un evento click
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault(); // Evitar el comportamiento predeterminado del enlace
+            const targetId = this.getAttribute('href').substring(1); // Obtener el ID del objetivo del enlace
+            smoothScroll(targetId); // Llamar a la función de desplazamiento suave
+        });
+    });
